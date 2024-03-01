038444eb360a3e8716c63677905b179708cd16ab
Rafat Siddiqui <mdrafat.siddiqui@outlook.com>
2024-03-01 19:28:10 +0530

    Initial Commit

---
+++ Spline3D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spline3D
+//
+//  Created by Rafat on 01/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spline3D/Spline3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spline3DApp.swift
+//  Spline3D
+//
+//  Created by Rafat on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Spline3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
